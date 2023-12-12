9732c15c9828b2abbbb18435150e6be7e8f563fd
subhadeep <srsubhadeepghosh456@gmail.com>
2023-12-12 12:04:14 +0530

    DOM Traversing

--- script.js
+++ script.js
@@ -166,3 +166,31 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+const h1 = document.querySelector("h1");
+//going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+//Going Upwards :Parents
+console.log(h1.parentElement);
+console.log(h1.parentNode);
+
+h1.closest(".header").style.backgroundColor = "yellow";
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+// all the siblings
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (element) {
+  if (element !== h1) {
+    element.style.transform = "scale(0.6)";
+  }
+});
+
+//Tabbed Components
