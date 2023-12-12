8b0ab2535ac091d2ce52e6807f3b95382edd7e04
subhadeep <srsubhadeepghosh456@gmail.com>
2023-12-12 21:54:19 +0530

    cool animation  Implemented

--- script.js
+++ script.js
@@ -218,3 +218,31 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach(function (el) {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+const nav = document.querySelector(".nav");
+
+nav.addEventListener("mouseover", function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", function (e) {
+  handleHover(e, 1);
+});
